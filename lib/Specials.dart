81f27889e89b71e5db8ec49360591d0005f444e5
Gabriel Luis Luna <lunagabo152@gmail.com>
2024-07-22 00:12:55 +0800

    added files to repository

---
+++ lib/Specials.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'main.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+class Specials extends StatelessWidget {
+  const Specials({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+      ),
+      
+      body: const Center(
+      child: Text(
+        'Desserts',
+        style: TextStyle(fontSize: 24),
+      ),
+      ),
+    );
+}
+
+
+}
+      
+  
